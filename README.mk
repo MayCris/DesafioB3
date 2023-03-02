8655eaea585d8c989bf79041f68b360faf77877c
MayCris <37078044+MayCris@users.noreply.github.com>
2023-03-02 10:35:16 -0300

    Adicionando arquivo readme.mk

---
+++ README.mk
@@ -0,0 +1,7 @@
+Abrir a solution no Visual Studio. A versão utilizada foi o 2022.
+Executar o projeto DesafioAPI
+Com o botão direito no projeto DesafioAngular, selecionar opção Abrir Terminal
+Inserir o código ng server.
+Após exibir mensagem de compilado com suscesso, abrir o navegador e acessat http://localhost:4200
+Preencher os dados de valor aplicação e meses e selecionar a opção Calcular.
+Os valores Bruto e Líquido serão preenchidos.
